890029f70028cbe5099bcc0dcc228cc70363fe9b
Kayro Pereira <kayrofellyxbr@gmail.com.br>
2020-05-21 21:48:31 -0300

    Ajuste de layout na activity principal Implementado recyclerView

--- app/src/main/java/com/example/applistadecompras/MainActivity.java
+++ app/src/main/java/com/example/applistadecompras/MainActivity.java
@@ -12,6 +12,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Produto> produtos;
+    private TabLayout tabLayout;
+
+    private int[] tabIcons = {
+            R.drawable.none_day,
+            R.drawable.rain,
+            R.drawable.none_night
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +33,28 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.vp1);
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
 
-        /*
+/*
         RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
         //rv.setLayoutManager(new LinearLayoutManager(this));
 
         produtos = new Produto().createContactsList(20);
 
-        ProdutoAdapter adapter = new ProdutoAdapter(produtos, this);
-        rv.setAdapter(adapter);
+        ProdutoAdapter adapter1 = new ProdutoAdapter(produtos, this);
+        rv.setAdapter(adapter1);
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         //rv.setItemViewCacheSize(produtos.size());
-         */
+ */
+
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
 }
 
--- app/src/main/java/com/example/applistadecompras/tela1.java
+++ app/src/main/java/com/example/applistadecompras/tela1.java
@@ -4,19 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class tela1 extends Fragment {
 
-    private TextView tv1;
-
-    private int i;
+    private ArrayList<Produto> produtos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.tela1, container, false);
+
+        RecyclerView rv = view.findViewById(R.id.rv);
+        //rv.setLayoutManager(new LinearLayoutManager(this));
+
+        produtos = new Produto().createContactsList(20);
+
+        //ProdutoAdapter adapter = new ProdutoAdapter(produtos, this);
+        ProdutoAdapter adapter = new ProdutoAdapter(produtos, getContext());
+        rv.setAdapter(adapter);
+        //rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        //rv.setItemViewCacheSize(produtos.size());
+        /*
         i = 0;
         View view = inflater.inflate(R.layout.tela1, container, false);
 
@@ -33,6 +48,7 @@ public class tela1 extends Fragment {
                 tv1.setText("Cont: " + i++);
             }
         });
+         */
 
         return view;
     }
